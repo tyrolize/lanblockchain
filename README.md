d9ea7dd8d130da0ba8fa926767956fbbe50a72f3
artemiyburov <artemiy.burov@outlook.com>
2022-12-15 20:31:02 +0100

    added chat bot and blockchain

--- README.md
+++ README.md
@@ -3,5 +3,14 @@ Blockchain experiments with Kathara
 
 ifconfig
 
+create pcs:
 kathara vstart --bridged -n pc1 --eth 0:A
-kathara vstart --bridged -n pc2 --eth 0:A
+
+open pc terminal:
+kathara connect -v pc1
+
+change to folder on a kathara machine:
+cd hosthome/prog/lanblockchain/cryptographisches_manifesto/pc_1/
+
+p2p part is inspired by https://github.com/macsnoeren/python-p2p-network
+crypto part is inspired by https://github.com/willassad/cryptocurrencypython
---
+++ cryptographisches_manifesto/pc_2/chat_bot.py
@@ -0,0 +1,37 @@
+import sys
+import time
+sys.path.insert(0, '..') # Import the files where the modules are located
+
+from p2p_node import MyOwnPeer2PeerNode
+
+node = MyOwnPeer2PeerNode("172.17.0.3", 8080, 2)
+
+
+time.sleep(5)
+
+node.start()
+
+
+time.sleep(5)
+
+debug = False
+node.debug = debug
+
+
+
+node.connect_with_node('172.17.0.2', 8080)
+
+
+time.sleep(5)
+
+node.send_to_nodes("message: Hi there! I am PC2, yo")
+
+while True:
+    #send 1 message
+    msg = input()
+    if msg == "kill":
+        break
+    else:
+        node.send_to_nodes(msg)
+
+node.stop()
